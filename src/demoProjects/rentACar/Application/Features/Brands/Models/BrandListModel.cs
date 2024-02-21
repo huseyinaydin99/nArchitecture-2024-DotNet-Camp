2fa2b692a5fac0645532a1ed912398056cc7c6e8
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-02-21 13:18:17 +0300

    Brand için CQRS sorgusu yazıldı AutoMapper eşlemesi yapıldı. Brand için DTO ve model sınıfları yazıldı.

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Models/BrandListModel.cs
@@ -0,0 +1,14 @@
+﻿using Core.Persistence.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Models;
+
+public class BrandListModel : BasePageableModel
+{
+    public IList<BrandListDto> Items { get; set; }
+
+}
---
+++ src/demoProjects/rentACar/Application/Features/Brands/Queries/GetListBrands/GetListBrandQuery.cs
@@ -0,0 +1,39 @@
+﻿using Application.Features.Brands.Models;
+using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Queries.GetListBrands;
+
+public class GetListBrandQuery : IRequest<BrandListModel>
+{
+    public PageRequest PageRequest { get; set; }
+    public class GetListBrandQueryHandler : IRequestHandler<GetListBrandQuery, BrandListModel>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+        private readonly BrandBusinessRules _brandBusinessRules;
+
+        public GetListBrandQueryHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRules)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+            _brandBusinessRules = brandBusinessRules;
+        }
+        public async Task<BrandListModel> Handle(GetListBrandQuery request, CancellationToken cancellationToken)
+        {
+            IPaginate<Brand> brands = await _brandRepository.GetListAsync(index: request.PageRequest.Page, size: request.PageRequest.PageSize);
+            BrandListModel mappedBrandListModel = _mapper.Map<BrandListModel>(brands);
+            return mappedBrandListModel;
+        }
+    }
+}
